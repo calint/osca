b7f33a505a23b9ebaaf139ab98e7972782ccf509
calin tenitchi <calin.tenitchi@gmail.com>
2016-03-07 17:19:27 +0700

    .

--- make-env.sh
+++ make-env.sh
@@ -3,5 +3,5 @@ I=arch/x86/boot/bzImage
 F=rootfs.cpio
 
 B=busybox
-CONFIG_LINUX=linux-configs/5-intel64-qemu-e1000-xiinux-config
+CONFIG_LINUX=linux-configs/7-intel64-qemu-e1000-xiinux-config
 CONFIG_BUSYBOX=busybox-configs/0.config
