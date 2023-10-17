e268a73ddfdeae8bfbb291ca2a0b542f10badb89
calint <calin.tenitchi@gmail.com>
2023-10-17 22:03:52 +0200

    .

--- frameless/make.sh
+++ frameless/make.sh
@@ -1,5 +1,10 @@
-#CC=cc
-CC="cc -std=c99"
+#!/bin/sh
+set -e
+
+# change to directory of the script
+cd $(dirname "$0")
+
+CC="cc"
 BIN=frameless
 SRC=src/frameless.c
 # ! -Os breaks moving and resizing windows
@@ -9,12 +14,13 @@ WARNINGS="-Wall -Wextra -Wno-unused-result -Wno-maybe-uninitialized"
 #WARNINGS="-Wall -Wextra -Wno-unused-result"
 LIBS=-lX11
 
-echo &&
-$CC -o $BIN  $SRC $LIBS $OPTS $WARNINGS &&
-echo    "             lines   words  chars" &&
-echo -n "   source:" &&
-cat $SRC|wc
-echo -n "   zipped:" &&
-cat $SRC|gzip|wc &&
-echo && ls -o --color $BIN &&
+echo
+$CC -o $BIN  $SRC $LIBS $OPTS $WARNINGS
+echo    "            lines   words   chars"
+echo -n "   source:"
+cat $SRC | wc
+echo -n "   zipped:"
+cat $SRC | gzip | wc
+echo
+ls -o --color $BIN
 echo
