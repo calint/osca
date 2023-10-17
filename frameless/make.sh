282c7ad5eb3f9e42599bf6ef0a9996b2963609ea
calint <calin.tenitchi@gmail.com>
2023-10-17 20:05:29 +0200

    .

--- frameless/make.sh
+++ frameless/make.sh
@@ -1,9 +1,3 @@
-#!/bin/sh
-set -e
-
-# change to directory of the script
-cd $(dirname "$0")
-
 #CC=cc
 CC="cc -std=c99"
 BIN=frameless
@@ -11,16 +5,16 @@ SRC=src/frameless.c
 # ! -Os breaks moving and resizing windows
 #OPTS="-Os -pedantic-errors -Wfatal-errors"
 OPTS="-pedantic-errors -Wfatal-errors"
-WARNINGS="-Wall -Wextra -Wpedantic -Wno-unused-result -Wno-maybe-uninitialized"
+WARNINGS="-Wall -Wextra -Wno-unused-result -Wno-maybe-uninitialized"
 #WARNINGS="-Wall -Wextra -Wno-unused-result"
 LIBS=-lX11
 
-echo
-$CC -o $BIN  $SRC $LIBS $OPTS $WARNINGS
-echo    "             lines  words   chars"
-echo -n "   source:"
-cat $SRC | wc
-echo -n "   zipped:"
-cat $SRC | gzip | wc
-echo && ls -o --color $BIN
+echo &&
+$CC -o $BIN  $SRC $LIBS $OPTS $WARNINGS &&
+echo    "             lines   words  chars" &&
+echo -n "   source:" &&
+cat $SRC|wc
+echo -n "   zipped:" &&
+cat $SRC|gzip|wc &&
+echo && ls -o --color $BIN &&
 echo
