f0e49168bea834ee6ae0e9895afd90b2b275d6c7
calint <calin.tenitchi@gmail.com>
2023-10-17 20:15:49 +0200

    .

--- frameless/src/frameless.c
+++ frameless/src/frameless.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
 #define logfile "frameless.log"
 #define bin_screenshot_area                                                    \
   "scrot -s 'scr--%Y-%m-%d---%H-%M-%S.jpg' -e 'mkdir -p ~/img/&&mv $f "        \
@@ -342,7 +344,7 @@ static void desksave(int dsk, FILE *f) {
     else
       fprintf(flog, "%x", (unsigned int)w->w);
     //		fprintf(f,"   %x %dx%d+%d+%d\n",(unsigned
-    //int)w->w,w->rw,w->rh,w->rx,w->ry);
+    // int)w->w,w->rw,w->rh,w->rx,w->ry);
     fflush(f);
   }
 }
@@ -408,6 +410,9 @@ int main(int argc, char **args, char **env) {
   XGrabKey(dpy, 123, 0, root, True, GrabModeAsync, GrabModeAsync); // volup
   XGrabKey(dpy, 107, 0, root, True, GrabModeAsync, GrabModeAsync); // print
   XSelectInput(dpy, root, SubstructureNotifyMask);
+  XButtonEvent buttonevstart;
+  memset(&buttonevstart, 0, sizeof(buttonevstart));
+  int dskprv = 0;
   XEvent ev;
   while (!XNextEvent(dpy, &ev)) {
     xwin *xw;
@@ -499,10 +504,11 @@ int main(int argc, char **args, char **env) {
         fflush(flog);
         if (pid == 0) { // child
           //					int
-          //r=execl("/usr/bin/scrot","-s","scr--%Y-%m-%d---%H-%M-%S.jpg","-e","mkdir
+          // r=execl("/usr/bin/scrot","-s","scr--%Y-%m-%d---%H-%M-%S.jpg","-e","mkdir
           //-p ~/img/&&mv $f ~/img/&&feh ~/img/$f",NULL);
           //					fprintf(flog,"screenshot rect:
-          //%d\n",r); 					fflush(flog); 					exit(r);
+          //%d\n",r); 					fflush(flog);
+          // exit(r);
           fprintf(flog, "exec scrot -s\n");
           int r = execlp("scrot", "-s", NULL);
           fprintf(flog, " after exec:  %d\n", r);
@@ -606,10 +612,8 @@ int main(int argc, char **args, char **env) {
         //				XSetCloseDownMode(dpy,RetainPermanent);
         //				XCloseDisplay(dpy);
         break;
-
-        int dskprv; //? weirddeclarelocation
-      case 38:      // a
-      case 111:     // up
+      case 38:  // a
+      case 111: // up
         dskprv = dsk;
         dsk++;
         if (ev.xkey.state & ShiftMask)
@@ -641,7 +645,6 @@ int main(int argc, char **args, char **env) {
         key = 0;
       break;
 
-      XButtonEvent buttonevstart; //? decllocation
     case ButtonPress:
       dragging = 1;
       xw = xwinget(ev.xbutton.window);
