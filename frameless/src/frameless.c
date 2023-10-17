bba8b0e15ed56b577ee3cd19836e8b3cb5bea5a0
calint <calin.tenitchi@gmail.com>
2023-10-17 23:17:17 +0200

    .

--- frameless/src/frameless.c
+++ frameless/src/frameless.c
@@ -10,20 +10,26 @@
 #include <string.h>
 #include <unistd.h>
 
+#define WIN_MAX_COUNT 128  // maximum number of windows
+#define WIN_SLIP 7         // pixels to show of a window folded to the right
+#define WIN_BORDER_WIDTH 1 // window border width
+#define WIN_BUMP 59        // pixels to 'bump' a window
+#define WIN_BORDER_ACTIVE_COLOR 0x00008000
+#define WIN_BORDER_INACTIVE_COLOR 0x00000000
+
 typedef int xdesk;
-typedef unsigned int bits;
+typedef unsigned bits;
+typedef char bool;
 typedef struct xwin {
-  Window w;            // x11 window handle
-  int gx, gy;          // position
-  unsigned int gw, gh; // width height
-  bits vh;             // bit 1 means fullheight    bit 2 means fullwidth
-  xdesk desk;          // desk the window is on
-  int desk_x;          // x coord of window before folded at desk switch
-  unsigned char bits;  // bit 1 means allocated
+  Window w;           // x11 window handle
+  int gx, gy;         // position
+  unsigned gw, gh;    // width height
+  bits vh;            // bit 1 means fullheight    bit 2 means fullwidth
+  xdesk desk;         // desk the window is on
+  int desk_x;         // x coord of window before folded at desk switch
+  unsigned char bits; // bit 1 means allocated
 } xwin;
-#define xwinsct 128 //? hardcoded
-#define border_width 1
-static xwin wins[xwinsct];
+static xwin wins[WIN_MAX_COUNT];
 static FILE *flog;
 static Display *dpy;
 static Window root;
@@ -32,13 +38,9 @@ static int wincount = 0;
 static struct scr {
   int id, wi, hi;
 } scr;
-// static int scr=0;
-// static int scr_w=0;
-// static int scr_h=0;
-static unsigned int key = 0;
-static const int winslip = 7;
+static unsigned key = 0;
 static xwin *winfocused = NULL;
-static int dragging = 0;
+static bool dragging = False;
 static char *ix_evnames[LASTEvent] = {
     "unknown",          "unknown",       // 0
     "KeyPress",         "KeyRelease",    // 2
@@ -58,17 +60,16 @@ static char *ix_evnames[LASTEvent] = {
     "GenericEvent"};
 
 static xwin *xwinget(Window w) {
-  int n;
   xwin *xw = NULL;
   int firstavail = -1;
-  for (n = 0; n < xwinsct; n++) {
-    if (wins[n].bits & 1) { //? magicnum1
-      if (wins[n].w == w) {
-        return &wins[n];
+  for (unsigned i = 0; i < WIN_MAX_COUNT; i++) {
+    if (wins[i].bits & 1) { // check if allocated
+      if (wins[i].w == w) { // same handle?
+        return &wins[i];
       }
     } else {
       if (firstavail == -1) {
-        firstavail = n;
+        firstavail = i;
       }
     }
   }
@@ -81,31 +82,24 @@ static xwin *xwinget(Window w) {
   xw = &wins[firstavail];
   xw->bits |= 1; // allocated
   wincount++;
-  fprintf(flog, "       wincount: %d\n", wincount);
-  fflush(flog);
   xw->w = w;
   xw->vh = 0;
   xw->desk = dsk;
-  XSetWindowBorderWidth(dpy, w, border_width);
+  XSetWindowBorderWidth(dpy, w, WIN_BORDER_WIDTH);
   return xw;
 }
 static void xwinfocus(xwin *this) {
-  // fprintf(flog, "xwinfocus  %p\n", (void *)this->w);
-  // fflush(flog);
   if (winfocused) {
-    XSetWindowBorder(dpy, winfocused->w, 0x00000000); //? magicnum
+    XSetWindowBorder(dpy, winfocused->w, WIN_BORDER_INACTIVE_COLOR);
   }
   XSetInputFocus(dpy, this->w, RevertToParent, CurrentTime);
-  XSetWindowBorder(dpy, this->w, 0x00008000); //? magicnum
-  // fprintf(flog, "xwinfocus  .\n");
-  // fflush(flog);
+  XSetWindowBorder(dpy, this->w, WIN_BORDER_ACTIVE_COLOR);
   winfocused = this;
 }
 static void xwinraise(xwin *this) { XRaiseWindow(dpy, this->w); }
 static void focusfirstondesk() {
-  int k;
-  for (k = 0; k < xwinsct; k++) {
-    xwin *w = &wins[k];
+  for (unsigned i = 0; i < WIN_MAX_COUNT; i++) {
+    xwin *w = &wins[i];
     if ((w->bits & 1) && (w->desk == dsk)) {
       xwinraise(w);
       xwinfocus(w);
@@ -115,34 +109,23 @@ static void focusfirstondesk() {
   winfocused = NULL;
 }
 static xwin *_winfind(Window w) {
-  // fprintf(flog, "! xwinfind  %p\n", (void *)w);
-  // fflush(flog);
-  int n;
-  for (n = 0; n < xwinsct; n++) {
-    if (wins[n].w == w)
-      return &wins[n];
+  for (unsigned i = 0; i < WIN_MAX_COUNT; i++) {
+    if (wins[i].w == w)
+      return &wins[i];
   }
   return NULL;
 }
 static void xwinfree(Window w) {
-  // fprintf(flog, "   xwinfree   %p\n", (void *)w);
-  // fflush(flog);
   xwin *xw = _winfind(w);
   if (!xw) {
-    // fprintf(flog, "! did not find window %p\n", (void *)w);
     return;
   }
-  // fprintf(flog, "       found xwin   %p    bits: %x\n", (void *)xw, xw->bits);
-  fflush(flog);
-  if (xw->bits & 1) {
-    xw->bits &= 0xfe; // free  //? magicnum
+  if (xw->bits & 1) { // if allocateds
+    xw->bits &= 0xfe; // mark free
     wincount--;
-    // fprintf(flog, "          freed    wincount: %d\n", wincount);
-    // fflush(flog);
   }
   if (winfocused == xw) {
     winfocused = NULL;
-    //		focusfirstondesk();
   }
 }
 static void xwingeom(xwin *this) {
@@ -157,9 +140,6 @@ static void xwingeomset(xwin *this, int x, int y, int w, int h) {
 static void xwingeomset2(xwin *this) {
   XMoveResizeWindow(dpy, this->w, this->gx, this->gy, this->gw, this->gh);
 }
-// static void xwinlower(xwin*this){
-//	XLowerWindow(dpy,this->w);
-// }
 static void xwingeomcenter(xwin *this) {
   xwingeom(this);
   int nx = (scr.wi - this->gw) >> 1; //? magicnum
@@ -194,7 +174,7 @@ static void xwintogglefullscreen(xwin *this) {
     this->vh = 0;
   } else { // toggle to fullscreen
     xwingeom(this);
-    xwingeomset(this, -border_width, -border_width, scr.wi, scr.hi);
+    xwingeomset(this, -WIN_BORDER_WIDTH, -WIN_BORDER_WIDTH, scr.wi, scr.hi);
     this->vh = 3;
   }
 }
@@ -206,7 +186,7 @@ static void xwintogglefullheight(xwin *this) {
     xwingeomset(this, this->gx, gy, this->gw, gh);
   } else {
     xwingeom(this);
-    xwingeomset(this, this->gx, -border_width, this->gw, scr.hi);
+    xwingeomset(this, this->gx, -WIN_BORDER_WIDTH, this->gw, scr.hi);
   }
   this->vh ^= 1;
 }
@@ -218,14 +198,14 @@ static void xwintogglefullwidth(xwin *this) {
     xwingeomset(this, gx, this->gy, gw, this->gh);
   } else {
     xwingeom(this);
-    xwingeomset(this, -border_width, this->gy, scr.wi, this->gh);
+    xwingeomset(this, -WIN_BORDER_WIDTH, this->gy, scr.wi, this->gh);
   }
   this->vh ^= 2;
 }
 static void xwinhide(xwin *this) {
   xwingeom(this);
   this->desk_x = this->gx;
-  int slip = rand() % winslip;
+  int slip = rand() % WIN_SLIP;
   this->gx = (scr.wi - 13 + slip); //? magicnum
   xwingeomset2(this);
 }
@@ -243,7 +223,7 @@ static int _xwinix(xwin *this) {
   if (this == NULL)
     return -1;
   int k;
-  for (k = 0; k < xwinsct; k++)
+  for (k = 0; k < WIN_MAX_COUNT; k++)
     if (this == &wins[k])
       return k;
   return -1;
@@ -251,7 +231,6 @@ static int _xwinix(xwin *this) {
 static int _focustry(int k) {
   xwin *w = &wins[k];
   if ((w->bits & 1) && (w->desk == dsk)) {
-    // fprintf(flog, "     focustry %p\n", (void *)w->w);
     xwinraise(w);
     xwinfocus(w);
     return 1;
@@ -261,7 +240,7 @@ static int _focustry(int k) {
 static void focusnext() {
   int k0 = _xwinix(winfocused);
   int k = k0;
-  while (++k < xwinsct) {
+  while (++k < WIN_MAX_COUNT) {
     if (_focustry(k)) {
       return;
     }
@@ -283,7 +262,7 @@ static void focusprev() {
       return;
     }
   }
-  k = xwinsct;
+  k = WIN_MAX_COUNT;
   while (--k >= 0) {
     if (_focustry(k)) {
       return;
@@ -308,7 +287,7 @@ static void togglefullwidth() {
 }
 static void deskshow(int dsk, int dskprv) {
   int n;
-  for (n = 0; n < xwinsct; n++) {
+  for (n = 0; n < WIN_MAX_COUNT; n++) {
     xwin *xw = &wins[n];
     if (!(xw->bits & 1)) //? magicnum
       continue;
@@ -357,29 +336,26 @@ static int errorhandler(Display *d, XErrorEvent *e) {
   return 0;
 }
 int main(int argc, char **args, char **env) {
-  while (argc--)
+  while (argc--) {
     puts(*args++);
-  while (*env)
+  }
+  while (*env) {
     puts(*env++);
+  }
   puts(APP);
   srand(0);
-  //	_Xdebug=1;
   XSetErrorHandler(errorhandler);
-  //	flog=stdout;
   flog = stdout; // fopen(logfile,"a");
-  if (!flog)
+  if (!flog) {
     exit(1);
+  }
   dpy = XOpenDisplay(NULL);
-  if (!dpy)
+  if (!dpy) {
     exit(2);
+  }
   scr.id = DefaultScreen(dpy);
   scr.wi = DisplayWidth(dpy, scr.id);
   scr.hi = DisplayHeight(dpy, scr.id);
-  // fprintf(flog, "\n\n\n\n%s\nscreen dimension: %d x %d\n", APP, scr.wi, scr.hi);
-  // fflush(flog);
-
-  for (unsigned i = 0; i < xwinsct; i++)
-    wins[i].bits = 0;
 
   root = DefaultRootWindow(dpy);
   Cursor root_window_cursor = XCreateFontCursor(dpy, XC_arrow);
@@ -415,59 +391,36 @@ int main(int argc, char **args, char **env) {
       //		case ConfigureNotify:break;
       //		case MapRequest:break;
     case MapNotify:
-      fprintf(flog, "mapnotify     %p\n", (void *)ev.xmap.window);
-      fflush(flog);
       if (ev.xmap.window == root || ev.xmap.window == 0 ||
           ev.xmap.override_redirect) {
-        fprintf(flog, "   ignored");
-        fflush(flog);
         break;
       }
       xw = xwinget(ev.xmap.window);
       xwingeomcenter(xw);
       xwinfocus(xw);
-      //			fprintf(flog,"mapnotify  1\n");fflush(flog);
       XGrabButton(dpy, AnyButton, Mod4Mask, xw->w, True, ButtonPressMask,
                   GrabModeAsync, GrabModeAsync, None, None);
-      //			fprintf(flog,"mapnotify  2\n");fflush(flog);
       XSelectInput(dpy, xw->w, EnterWindowMask);
-      fprintf(flog, "mapnotify  .\n");
-      fflush(flog);
       break;
     case UnmapNotify:
-      fprintf(flog, "unmapnotify   %p   %p\n", (void *)ev.xmap.window,
-              (void *)ev.xmap.window);
-      fflush(flog);
       if (ev.xmap.window == root || ev.xmap.window == 0 ||
           ev.xmap.override_redirect) {
-        fprintf(flog, "   ignored");
-        fflush(flog);
         break;
       }
       //			fprintf(flog,"unmapnotify  1\n");fflush(flog);
       xwinfree(ev.xmap.window);
-      fprintf(flog, "unmapnotify  .\n");
-      fflush(flog);
       break;
     case EnterNotify:
       if (dragging)
         break;
-      fprintf(flog, "enternotify   %p   root=%p   subwin=%p\n",
-              (void *)ev.xcrossing.window, (void *)ev.xcrossing.root,
-              (void *)ev.xcrossing.subwindow);
-      fflush(flog);
       xw = xwinget(ev.xcrossing.window);
       xwinfocus(xw);
-      fprintf(flog, "enternotify  .\n");
-      fflush(flog);
       break;
     case KeyPress:
-      fprintf(flog, "keypress   %p   root=%p   subwin=%p\n",
-              (void *)ev.xcrossing.window, (void *)ev.xcrossing.root,
-              (void *)ev.xcrossing.subwindow);
       key = ev.xkey.keycode;
-      if (ev.xkey.subwindow)
+      if (ev.xkey.subwindow) {
         xw = xwinget(ev.xkey.subwindow);
+      }
       switch (key) {
       case 53: // x
         system("xii-sticky");
@@ -479,33 +432,6 @@ int main(int argc, char **args, char **env) {
       case 33: // p
         system("xii-scrsht");
         break;
-      case 70: { // f4
-        fprintf(flog, "forking\n");
-        fflush(flog);
-        pid_t pid = fork();
-        fprintf(flog, "forked %d\n", pid);
-        fflush(flog);
-        if (pid == 0) { // child
-          //					int
-          // r=execl("/usr/bin/scrot","-s","scr--%Y-%m-%d---%H-%M-%S.jpg","-e","mkdir
-          //-p ~/img/&&mv $f ~/img/&&feh ~/img/$f",NULL);
-          //					fprintf(flog,"screenshot rect:
-          //%d\n",r); 					fflush(flog);
-          // exit(r);
-          fprintf(flog, "exec scrot -s\n");
-          int r = execlp("scrot", "-s", NULL);
-          fprintf(flog, " after exec:  %d\n", r);
-          fflush(flog);
-          // exit(r);
-          return r;
-        } else if (pid < 0) { // could not fork
-          fprintf(flog, "could not fork\n");
-          fflush(flog);
-        }
-        // #define bin_screenshot_area "scrot -s 'scr--%Y-%m-%d---%H-%M-%S.jpg'
-        // -e 'mkdir -p ~/img/&&mv $f ~/img/&&feh ~/img/$f'"
-        break;
-      }
       case 24: // q
         system("xii-qbin");
         break;
@@ -526,24 +452,28 @@ int main(int argc, char **args, char **env) {
         break;
       case 9:  // esc
       case 49: // §
-        if (winfocused)
+        if (winfocused) {
           xwinclose(winfocused);
+        }
         break;
       case 39: // s
-        if (winfocused)
+        if (winfocused) {
           xwingeomcenter(winfocused);
+        }
         break;
       case 25: // w
         if (winfocused) {
-          if (ev.xkey.state & ShiftMask)
+          if (ev.xkey.state & ShiftMask) {
             xwingeomthinner(winfocused);
-          else
+          } else {
             xwingeomwider(winfocused);
+          }
         }
         break;
       case 56: // b
-        if (winfocused)
-          xwinbump(winfocused, 59); //? magicnum
+        if (winfocused) {
+          xwinbump(winfocused, WIN_BUMP);
+        }
         break;
       case 12: // 3
         togglefullscreen();
@@ -554,8 +484,8 @@ int main(int argc, char **args, char **env) {
       case 14: // 5
         togglefullwidth();
         break;
-      case 15:                     // 6
-        xwinbump(winfocused, 200); //? magicnum
+      case 15: // 6
+        xwinbump(winfocused, 200);
         break;
       case 16: // 7
         system("xii-ide");
@@ -599,26 +529,28 @@ int main(int argc, char **args, char **env) {
       case 111: // up
         dskprv = dsk;
         dsk++;
-        if (ev.xkey.state & ShiftMask)
+        if (ev.xkey.state & ShiftMask) {
           if (winfocused) {
             winfocused->desk = dsk;
             xwingeom(winfocused);
             winfocused->desk_x = winfocused->gx;
             xwinraise(winfocused);
           }
+        }
         deskshow(dsk, dskprv);
         break;
       case 40:  // d
       case 116: // down
         dskprv = dsk;
         dsk--;
-        if (ev.xkey.state & ShiftMask)
+        if (ev.xkey.state & ShiftMask) {
           if (winfocused) {
             winfocused->desk = dsk;
             xwingeom(winfocused);
             winfocused->desk_x = winfocused->gx;
             xwinraise(winfocused);
           }
+        }
         deskshow(dsk, dskprv);
         break;
       }
@@ -628,7 +560,7 @@ int main(int argc, char **args, char **env) {
         key = 0;
       break;
     case ButtonPress:
-      dragging = 1;
+      dragging = True;
       xw = xwinget(ev.xbutton.window);
       xwinfocus(xw);
       XGrabPointer(dpy, xw->w, True, PointerMotionMask | ButtonReleaseMask,
@@ -647,15 +579,13 @@ int main(int argc, char **args, char **env) {
       int ny = xw->gy + ydiff;
       int nh = xw->gh + ydiff;
       if (xw->vh & 2) {
-        nx = -border_width;
+        nx = -WIN_BORDER_WIDTH;
         nw = scr.wi;
       }
       if (xw->vh & 1) {
-        ny = -border_width;
+        ny = -WIN_BORDER_WIDTH;
         nh = scr.hi;
       }
-      // fprintf(flog, "button=%d    btn1msk=%d    btn2msk=%d    btn3msk=%d\n",
-      //         buttonevstart.button, Button1Mask, Button2Mask, Button3Mask);
       if (buttonevstart.button == 3) {
         if (nw < 0)
           nw = 0;
@@ -678,13 +608,12 @@ int main(int argc, char **args, char **env) {
       }
       break;
     case ButtonRelease:
-      dragging = 0;
+      dragging = False;
       xw = xwinget(ev.xbutton.window);
       xw->desk = dsk;
       XUngrabPointer(dpy, CurrentTime);
       break;
     }
   }
-  //? cleanup  cursor
   return 0;
 }
