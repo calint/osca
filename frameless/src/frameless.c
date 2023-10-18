7acf84fdb16ce7f2ce703a62da77cd2068bf7fda
calint <calin.tenitchi@gmail.com>
2023-10-18 04:19:33 +0200

    .

--- frameless/src/frameless.c
+++ frameless/src/frameless.c
@@ -178,8 +178,8 @@ static void xwin_thinner(xwin *this) {
   xwin_read_geom(this);
   unsigned wi_prv = this->wi;
   this->wi = ((this->wi << 1) + this->wi) >> 2;
-  if (this->wi < 1) {
-    this->wi = 1;
+  if (this->wi < 2) {
+    this->wi = 2;
   }
   this->x = this->x - ((this->wi - wi_prv) >> 1);
   xwin_set_geom(this);
