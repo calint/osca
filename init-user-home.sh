0655ab3be22c011d78741bea1928b231892001e5
calint <calin.tenitchi@gmail.com>
2023-10-19 22:25:22 +0200

    .

--- init-user-home.sh
+++ init-user-home.sh
@@ -2,9 +2,9 @@
 set -e
 cd $(dirname "$0")
 
-echo * copy '~/w/osca/etc/skel/.' to '~/'
+echo · copy '~/w/osca/etc/skel/.' to '~/'
 cp -ra ~/w/osca/etc/skel/. ~/
-echo * load dconf from '~/.config/dconf.ini'
+echo · load dconf from '~/.config/dconf.ini'
 dconf load / < ~/.config/dconf.ini
-echo * delete '~/.config/dconf.ini'
+echo · delete '~/.config/dconf.ini'
 rm ~/.config/dconf.ini
