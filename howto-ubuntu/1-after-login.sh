748fd62955f98797f266a2324579085944d42b5e
calint <calin.tenitchi@gmail.com>
2023-10-21 08:43:30 +0200

    .

---
+++ howto-ubuntu/1-after-login.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# notes.
+# change .xinitrc
+# ln -s .xinitrc .xsession
+# systemctl status display-manager
+# sudo apt install mousepad thunar libinput-tools
+# touchpad
+# xinput - to list devices
+# xinput list-props [id]
+# look for "Tapping Enabled"
+xinput set-prop 10 305 1
+
+# look for "Natural Scrolling Enabled"
+xinput set-prop 10 284 1
+
+# use gnome-keyring-daemon to manage keys
+/usr/bin/gnome-keyring-daemon --start --components=gpg
+/usr/bin/gnome-keyring-daemon --start --components=ssh
+
