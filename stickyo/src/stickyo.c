7074dbda9a82523c0a82800f178bbd55ca19d0dc
calint <calin.tenitchi@gmail.com>
2023-11-03 19:25:21 +0100

    .

--- stickyo/src/stickyo.c
+++ stickyo/src/stickyo.c
@@ -3,8 +3,11 @@
 #define WIDTH 200
 #define HEIGHT 200
 
-static void activate(GtkApplication *app, gpointer user_data) {
-  GtkWidget *window = gtk_application_window_new(app);
+static void command_line(GApplication *app, GApplicationCommandLine *cmdline) {
+  gint argc;
+  gchar **argv = g_application_command_line_get_arguments(cmdline, &argc);
+
+  GtkWidget *window = gtk_application_window_new(GTK_APPLICATION(app));
   gtk_window_set_title(GTK_WINDOW(window), "stickyo");
   gtk_window_set_default_size(GTK_WINDOW(window), WIDTH, HEIGHT);
 
@@ -12,16 +15,11 @@ static void activate(GtkApplication *app, gpointer user_data) {
   gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(text_view), GTK_WRAP_WORD_CHAR);
   gtk_text_view_set_editable(GTK_TEXT_VIEW(text_view), TRUE);
 
-  const gchar *text = g_getenv("STICKYO_TEXT");
-
   // Set the text of the text view
-  if (text != NULL) {
+  if (argc > 1) {
     GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
-    gtk_text_buffer_set_text(buffer, text, -1);
+    gtk_text_buffer_set_text(buffer, argv[1], -1);
   }
-
-  // GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
-  // gtk_text_buffer_set_text(buffer, "Hello World", -1);
 
   const char *css = "textview { font: 9pt monospace; background-color: "
                     "yellow;padding: 5pt; } textview text { background: "
@@ -43,8 +41,8 @@ static void activate(GtkApplication *app, gpointer user_data) {
 
 int main(int argc, char **argv) {
   GtkApplication *app = gtk_application_new("com.github.calint.stickyo",
-                                            G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+                                            G_APPLICATION_HANDLES_COMMAND_LINE);
+  g_signal_connect(app, "command-line", G_CALLBACK(command_line), NULL);
   int status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
   return status;
