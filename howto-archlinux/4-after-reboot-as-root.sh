016cbfe2aa3f168ef3a83a96f5826f6843452917
calint <calin.tenitchi@gmail.com>
2023-11-05 16:44:56 +0100

    .

--- howto-archlinux/4-after-reboot-as-root.sh
+++ howto-archlinux/4-after-reboot-as-root.sh
@@ -2,6 +2,10 @@
 set -e
 cd $(dirname "$0")
 
+echo assuming ethernet is 'enp0s3'
+echo modify the script with actual ethernet interface if necessary
+echo run 'ip link' and find out
+
 ip link set enp0s3 up
 systemctl enable systemd-networkd
 systemctl start systemd-networkd
