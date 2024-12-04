9cab0073b24d33c828695dc0384765a6336317a0
calint <calin.tenitchi@gmail.com>
2024-12-04 21:24:23 +0700

    .

--- howto-archlinux/2024.12.01--archinstall/install-packages.sh
+++ howto-archlinux/2024.12.01--archinstall/install-packages.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-sudo pacman -S git nano libxft gtk4 thunar tumbler mousepad rhythmbox \
+sudo pacman -S git nano less libxft gtk4 thunar tumbler mousepad rhythmbox \
                alsa-utils xorg-xsetroot scrot xorg-xinput ttf-dejavu htop feh
-
-               
