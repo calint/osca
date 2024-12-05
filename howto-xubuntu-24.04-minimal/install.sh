6f6eb98b1cfa8438869426941050a0e60bed0600
calint <calin.tenitchi@gmail.com>
2024-12-05 12:33:44 +0700

    .

--- howto-xubuntu-24.04-minimal/install.sh
+++ howto-xubuntu-24.04-minimal/install.sh
@@ -8,17 +8,23 @@ sudo apt upgrade
 echo
 echo install frameless
 echo
+
+sudo apt install git
+
 mkdir w
 cd w
+
 git clone https://github.com/calint/osca
+
 sudo apt install gcc libx11-dev libxft-dev gnome-devel dconf-cli scrot \
                  mousepad alsamixergui dconf-cli thunar feh alsa-utils \
                  xbacklight vlc rhythmbox
 snap install brave
+
 cd osca/
 ./make-all.sh
-sudo cp -a usr/share/xsessions/frameless.desktop /usr/share/xsessions/
 ./init-user-home.sh
+sudo cp -a usr/share/xsessions/frameless.desktop /usr/share/xsessions/
 
 echo
 echo logout and login to xsession frameless
