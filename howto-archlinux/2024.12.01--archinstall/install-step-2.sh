07d468aa55aac984a1eac3cf3c006f6544862eda
calint <calin.tenitchi@gmail.com>
2024-12-11 14:23:16 +0700

    .

--- howto-archlinux/2024.12.01--archinstall/install-step-2.sh
+++ howto-archlinux/2024.12.01--archinstall/install-step-2.sh
@@ -3,7 +3,8 @@ set -e
 
 sudo pacman --needed -S nano less xterm libxft gtk4 thunar tumbler mousepad \
                         rhythmbox alsa-utils xorg-xsetroot scrot xorg-xinput \
-                        ttf-dejavu htop feh acpi sysstat bluez bluez-utils
+                        ttf-dejavu htop feh acpi sysstat bluez bluez-utils \
+                        brightnessctl man
 
 cd ~/w/osca
 ./init-user-home.sh
