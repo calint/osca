719553bd377832fc58106d9aee8d76907c88300b
calint <calin.tenitchi@gmail.com>
2024-12-15 18:38:09 +0700

    .

--- howto-archlinux/2024.12.01--archinstall/install-step-2.sh
+++ howto-archlinux/2024.12.01--archinstall/install-step-2.sh
@@ -3,7 +3,8 @@ set -e
 
 sudo pacman --needed -S xterm xorg-xsetroot xorg-xinput libxft thunar tumbler \
                         mousepad gtk4 rhythmbox mpv alsa-utils scrot feh eog \
-                        acpi sysstat brightnessctl ttf-dejavu bluez bluez-utils
+                        acpi sysstat brightnessctl ttf-dejavu bluez bluez-utils \
+                        gvfs gvfs-mtp
 
 cd ~/w/osca
 ./init-user-home.sh
