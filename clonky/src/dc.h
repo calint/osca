53b0ffc81e564a616e274a9f94825b1109d8e2cc
calint <calin.tenitchi@gmail.com>
2024-12-21 19:07:07 +0700

    .

--- clonky/src/dc.h
+++ clonky/src/dc.h
@@ -10,18 +10,23 @@ struct dc;
 
 void dc_del(/*takes*/ struct dc *self);
 
+// clears the draw area and resets the cursor to the top left corner
 void dc_clear(struct dc *self);
 
 void dc_draw_line(struct dc *self, int x0, int y0, int x1, int y1);
 
+// draws horizontal rule
 void dc_draw_hr(struct dc *self);
 
+// draws horizontal rule with custom width
 void dc_draw_hr1(struct dc *self, int width);
 
 void dc_draw_str(struct dc *self, const char *str);
 
+// moves the cursor to the next line
 void dc_newline(struct dc *self);
 
+// moves the cursor down by 'dy' pixels
 void dc_inc_y(struct dc *self, int dy);
 
 void dc_flush(const struct dc *self);
