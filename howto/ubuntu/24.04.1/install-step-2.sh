9216dc4cff8e023ab553e2ff0c157531d2cafe33
calint <calin.tenitchi@gmail.com>
2025-02-07 14:55:43 +0100

    .

--- howto/ubuntu/24.04.1/install-step-2.sh
+++ howto/ubuntu/24.04.1/install-step-2.sh
@@ -4,7 +4,7 @@ set -e
 sudo apt -y install gcc libx11-dev libxft-dev gnome-devel dconf-cli scrot \
                  mousepad alsamixergui dconf-cli thunar feh alsa-utils \
                  xbacklight vlc rhythmbox xterm acpi \
-                 ffmpegthumbnailer poppler-glib libgsf libgepub libopenraw freetype2
+                 ffmpegthumbnailer freetype2
 snap install brave
 sudo apt purge firefox
 sudo snap remove --purge firefox
