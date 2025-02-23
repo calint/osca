0ac5d7a3ad5162e62795d90828c902bf60e39055
calint <calin.tenitchi@gmail.com>
2025-02-23 12:00:45 +0100

    .

--- clonky/src/main.c
+++ clonky/src/main.c
@@ -245,40 +245,17 @@ static void pl(const char *str) {
 
 static void render_hr(void) { dc_draw_hr(dc); }
 
-static void render_battery(void) {
-  if (battery_name[0] == '\0') {
-    // if no battery on the system
+static void render_date_time(void) {
+  const time_t t = time(NULL);
+  const struct tm *lt = localtime(&t);
+  strb sb;
+  strb_init(&sb);
+  if (strb_p(&sb, asctime(lt))) {
     return;
   }
-  char path[128] = "";
-  const int nchars =
-      snprintf(path, sizeof(path), "%s%s/%s_", power_supply_path_prefix,
-               battery_name, battery_energy_or_charge_prefix);
-  if (nchars < 0 || (size_t)nchars >= sizeof(path)) {
-    return; // truncated
-  }
-  const size_t maxlen = sizeof(path) - (size_t)nchars;
-  // construct paths to '..._full' and '..._now'
-  char *path_prefix_end = path + nchars;
-  strncpy(path_prefix_end, "full", maxlen);
-  path[sizeof(path) - 1] = '\0'; // ensure null-termination
-  const long long charge_full = sys_value_long(path);
-  strncpy(path_prefix_end, "now", maxlen);
-  path[sizeof(path) - 1] = '\0'; // ensure null-termination
-  const long long charge_now = sys_value_long(path);
-  snprintf(path, sizeof(path), "%s%s/status", power_supply_path_prefix,
-           battery_name);
-  // read battery status
-  char status[64];
-  sys_value_str_line(path, status, sizeof(status));
-  str_to_lower(status);
-  // format output
-  char output[128];
-  snprintf(output, sizeof(output), "battery %u%%  %s",
-           (unsigned)(charge_now * 100 / charge_full), status);
-  // draw a separator for visual que of current battery charge
-  dc_draw_hr1(dc, (int)(WIDTH * charge_now / charge_full));
-  pl(output);
+  // delete the '\n' that asctime writes at the end of the string
+  strb_back(&sb);
+  pl(sb.chars);
 }
 
 static void render_cpu_load(void) {
@@ -368,251 +345,6 @@ static void render_mem_info(void) {
   pl(buf);
 }
 
-static void render_net_graph(void) {
-  if (net_device[0] == '\0') {
-    // if no network device to graph
-    dc_draw_hr(dc);
-    return;
-  }
-  dc_inc_y(dc, DEFAULT_GRAPH_HEIGHT + HR_PIXELS_BEFORE);
-  char path[128] = "";
-  snprintf(path, sizeof(path), "/sys/class/net/%s/statistics/tx_bytes",
-           net_device);
-  long long tx_bytes = sys_value_long(path);
-  snprintf(path, sizeof(path), "/sys/class/net/%s/statistics/rx_bytes",
-           net_device);
-  long long rx_bytes = sys_value_long(path);
-  graphd_add_value(graph_net, tx_bytes + rx_bytes);
-  graphd_draw(graph_net, dc, DEFAULT_GRAPH_HEIGHT, NET_GRAPH_MAX);
-}
-
-static void render_cheetsheet(void) {
-  static char *keysheet[] = {"ĸey",
-                             "+c               console",
-                             "+f                 files",
-                             "+e                editor",
-                             "+m                 media",
-                             "+v                 mixer",
-                             "+i              internet",
-                             "+x                sticky",
-                             "+o              binaries",
-                             "+p              snapshot",
-                             "+shift+p          ↳ area",
-                             "",
-                             "đesktop",
-                             "+up                   up",
-                             "+down               down",
-                             "",
-                             "window",
-                             "+esc               close",
-                             "+b                  bump",
-                             "+s                center",
-                             "+w                 wider",
-                             "+W               thinner",
-                             "+r                resize",
-                             "+1           full-screen",
-                             "+2           full-height",
-                             "+3            full-width",
-                             "+right        focus-next",
-                             "+left         focus-prev",
-                             "+shift+up        move-up",
-                             "+shift+down    move-down",
-                             "+0   i-am-bored-surprise",
-                             " ...                ... ",
-                             NULL};
-
-  char **str_ptr = keysheet;
-  while (*str_ptr) {
-    pl(*str_ptr);
-    str_ptr++;
-  }
-}
-
-static void render_df(void) {
-  FILE *file = popen("df -h", "r");
-  if (!file) {
-    return;
-  }
-  // df -h gives:
-  //  Filesystem      Size  Used Avail Use% Mounted on
-  //  tmpfs           1,6G  2,3M  1,6G   1% /run
-  //  /dev/nvme0n1p6  235G  166G   57G  75% /
-
-  char buf[256] = "";
-  unsigned counter = RENDER_DF_MAX_LINE_COUNT;
-  while (counter--) {
-    if (fscanf(file, "%255[^\n]%*c", buf) == EOF) {
-      break;
-    }
-    str_compact_spaces(buf);
-    if (buf[0] != '/') {
-      continue;
-    }
-    pl(buf);
-  }
-  pclose(file);
-}
-
-static void render_io_stat(void) {
-  static long long prv_kb_read_total = 0;
-  static long long prv_kb_written_total = 0;
-
-  FILE *file = popen("iostat -d", "r");
-  if (!file) {
-    return;
-  }
-  // Linux 6.2.0-34-generic (c) 	2023-10-20 	_x86_64_	(12 CPU)
-  // Device  tps  kB_read/s  kB_wrtn/s  kB_dscd/s  kB_read  kB_wrtn  kB_dscd
-  //
-  // loop0   0,00 0,00       0,00       0,00       21       0        0
-
-  char buf[256] = "";
-  // read the header
-  fgets(buf, sizeof(buf), file);
-  fgets(buf, sizeof(buf), file);
-  fgets(buf, sizeof(buf), file);
-  // sum values
-  long long kb_read = 0;
-  long long kb_read_total = 0;
-  long long kb_written = 0;
-  long long kb_written_total = 0;
-  while (fgets(buf, sizeof(buf), file)) {
-    if (buf[0] == '\0') {
-      // break on empty line
-      break;
-    }
-    char dev[64] = "";
-    sscanf(buf, "%63s %*s %*s %*s %*s %lld %lld", dev, &kb_read, &kb_written);
-    kb_read_total += kb_read;
-    kb_written_total += kb_written;
-  }
-  pclose(file);
-
-  const char *unit = "kB/s";
-  snprintf(buf, sizeof(buf), "read %lld %s wrote %lld %s",
-           kb_read_total -
-               (prv_kb_read_total ? prv_kb_read_total : kb_read_total),
-           unit,
-           kb_written_total -
-               (prv_kb_written_total ? prv_kb_written_total : kb_written_total),
-           unit);
-  pl(buf);
-
-  prv_kb_read_total = kb_read_total;
-  prv_kb_written_total = kb_written_total;
-}
-
-static void render_syslog(void) {
-  FILE *file = popen("journalctl -b -p6 -o cat -n 15 --no-pager", "r");
-  if (!file) {
-    return;
-  }
-  unsigned counter = RENDER_SYSLOG_MAX_LINE_COUNT;
-  while (counter--) {
-    char buf[512] = "";
-    if (fscanf(file, "%511[^\n]%*c", buf) == EOF) {
-      break;
-    }
-    pl(buf);
-  }
-  pclose(file);
-}
-
-static void render_acpi(void) {
-  FILE *file = popen("acpi -at", "r");
-  if (!file) {
-    return;
-  }
-  unsigned counter = RENDER_ACPI_MAX_LINE_COUNT;
-  while (counter--) {
-    char buf[512] = "";
-    if (fscanf(file, "%511[^\n]%*c", buf) == EOF) {
-      break;
-    }
-    str_to_lower(buf);
-    pl(buf);
-  }
-  pclose(file);
-}
-
-static void render_date_time(void) {
-  const time_t t = time(NULL);
-  const struct tm *lt = localtime(&t);
-  strb sb;
-  strb_init(&sb);
-  if (strb_p(&sb, asctime(lt))) {
-    return;
-  }
-  // delete the '\n' that asctime writes at the end of the string
-  strb_back(&sb);
-  pl(sb.chars);
-}
-
-static void render_cores_throttle(void) {
-  FILE *file = fopen("/sys/devices/system/cpu/present", "r");
-  if (!file) {
-    return;
-  }
-  unsigned min = 0;
-  unsigned max = 0;
-  fscanf(file, "%u-%u", &min, &max);
-  fclose(file);
-
-  strb sb;
-  strb_init(&sb);
-  if (strb_p(&sb, "throttle ")) {
-    return;
-  }
-  const unsigned ncpus = max - min + 1;
-  strb_p_uint(&sb, ncpus);
-  strb_p(&sb, " core");
-  if (ncpus != 1) {
-    strb_p_char(&sb, 's');
-  }
-  if (ncpus > 2) {
-    // if more than 2 cpus display throttles on new line
-    pl(sb.chars);
-    strb_clear(&sb);
-  }
-  const unsigned ncols = 5;
-  unsigned cpu_ix = min;
-  unsigned total_proc = 0;
-  while (cpu_ix <= max) {
-    for (unsigned col = 0; col < ncols && cpu_ix <= max; col++) {
-      char path[128] = "";
-      snprintf(path, sizeof(path),
-               "/sys/devices/system/cpu/cpu%u/cpufreq/scaling_max_freq",
-               cpu_ix);
-      const long long max_freq = sys_value_long(path);
-      snprintf(path, sizeof(path),
-               "/sys/devices/system/cpu/cpu%u/cpufreq/scaling_cur_freq",
-               cpu_ix);
-      const long long cur_freq = sys_value_long(path);
-      strb_p_char(&sb, ' ');
-      if (max_freq) {
-        // if available render percent of max frequency
-        const unsigned proc = (unsigned)(cur_freq * 100 / max_freq);
-        total_proc += proc;
-        strb_p_int_with_width(&sb, (int)proc, 3);
-        strb_p_char(&sb, '%');
-      } else {
-        // max frequency not available
-        strb_p(&sb, "----");
-      }
-      cpu_ix++;
-    }
-    pl(sb.chars);
-    strb_clear(&sb);
-  }
-  if (ncpus != 0 && total_proc != 0) {
-    strb_p(&sb, "average: ");
-    strb_p_uint(&sb, (total_proc + (ncpus / 2)) / ncpus);
-    // note: ncpus / 2 for rounding to nearest integer
-    strb_p_char(&sb, '%');
-    pl(sb.chars);
-  }
-}
-
 static void render_swaps(void) {
   FILE *file = fopen("/proc/swaps", "r");
   if (!file) {
@@ -640,6 +372,24 @@ static void render_swaps(void) {
     return;
   }
   pl(sb.chars);
+}
+
+static void render_net_graph(void) {
+  if (net_device[0] == '\0') {
+    // if no network device to graph
+    dc_draw_hr(dc);
+    return;
+  }
+  dc_inc_y(dc, DEFAULT_GRAPH_HEIGHT + HR_PIXELS_BEFORE);
+  char path[128] = "";
+  snprintf(path, sizeof(path), "/sys/class/net/%s/statistics/tx_bytes",
+           net_device);
+  long long tx_bytes = sys_value_long(path);
+  snprintf(path, sizeof(path), "/sys/class/net/%s/statistics/rx_bytes",
+           net_device);
+  long long rx_bytes = sys_value_long(path);
+  graphd_add_value(graph_net, tx_bytes + rx_bytes);
+  graphd_draw(graph_net, dc, DEFAULT_GRAPH_HEIGHT, NET_GRAPH_MAX);
 }
 
 static struct netifc *netifcs_get_by_name_or_create(const char *name) {
@@ -822,17 +572,189 @@ static void render_net_interfaces(void) {
   freeifaddrs(ifas);
 }
 
-static void render_top_10_processes(void) {
-  FILE *file = popen("ps -eo %mem,%cpu,comm --sort=-%mem | head -n 11", "r");
+static void render_io_stat(void) {
+  static long long prv_kb_read_total = 0;
+  static long long prv_kb_written_total = 0;
+
+  FILE *file = popen("iostat -d", "r");
   if (!file) {
     return;
   }
-  char buf[512];
-  unsigned counter = 11;
+  // Linux 6.2.0-34-generic (c) 	2023-10-20 	_x86_64_	(12 CPU)
+  // Device  tps  kB_read/s  kB_wrtn/s  kB_dscd/s  kB_read  kB_wrtn  kB_dscd
+  //
+  // loop0   0,00 0,00       0,00       0,00       21       0        0
+
+  char buf[256] = "";
+  // read the header
+  fgets(buf, sizeof(buf), file);
+  fgets(buf, sizeof(buf), file);
+  fgets(buf, sizeof(buf), file);
+  // sum values
+  long long kb_read = 0;
+  long long kb_read_total = 0;
+  long long kb_written = 0;
+  long long kb_written_total = 0;
+  while (fgets(buf, sizeof(buf), file)) {
+    if (buf[0] == '\0') {
+      // break on empty line
+      break;
+    }
+    sscanf(buf, "%*s %*s %*s %*s %*s %lld %lld", &kb_read, &kb_written);
+    kb_read_total += kb_read;
+    kb_written_total += kb_written;
+  }
+  pclose(file);
+
+  snprintf(
+      buf, sizeof(buf), "read %lld kB/s wrote %lld kB/s",
+      kb_read_total - (prv_kb_read_total ? prv_kb_read_total : kb_read_total),
+      kb_written_total -
+          (prv_kb_written_total ? prv_kb_written_total : kb_written_total));
+  pl(buf);
+
+  prv_kb_read_total = kb_read_total;
+  prv_kb_written_total = kb_written_total;
+}
+
+static void render_df(void) {
+  FILE *file = popen("df -h", "r");
+  if (!file) {
+    return;
+  }
+  // df -h gives:
+  //  Filesystem      Size  Used Avail Use% Mounted on
+  //  tmpfs           1,6G  2,3M  1,6G   1% /run
+  //  /dev/nvme0n1p6  235G  166G   57G  75% /
+
+  char buf[256] = "";
+  unsigned counter = RENDER_DF_MAX_LINE_COUNT;
   while (counter--) {
+    if (fscanf(file, "%255[^\n]%*c", buf) == EOF) {
+      break;
+    }
+    str_compact_spaces(buf);
+    if (buf[0] != '/') {
+      continue;
+    }
+    pl(buf);
+  }
+  pclose(file);
+}
+
+static void render_cores_throttle(void) {
+  FILE *file = fopen("/sys/devices/system/cpu/present", "r");
+  if (!file) {
+    return;
+  }
+  unsigned min = 0;
+  unsigned max = 0;
+  fscanf(file, "%u-%u", &min, &max);
+  fclose(file);
+
+  strb sb;
+  strb_init(&sb);
+  if (strb_p(&sb, "throttle ")) {
+    return;
+  }
+  const unsigned ncpus = max - min + 1;
+  strb_p_uint(&sb, ncpus);
+  strb_p(&sb, " core");
+  if (ncpus != 1) {
+    strb_p_char(&sb, 's');
+  }
+  if (ncpus > 2) {
+    // if more than 2 cpus display throttles on new line
+    pl(sb.chars);
+    strb_clear(&sb);
+  }
+  const unsigned ncols = 5;
+  unsigned cpu_ix = min;
+  unsigned total_proc = 0;
+  while (cpu_ix <= max) {
+    for (unsigned col = 0; col < ncols && cpu_ix <= max; col++) {
+      char path[128] = "";
+      snprintf(path, sizeof(path),
+               "/sys/devices/system/cpu/cpu%u/cpufreq/scaling_max_freq",
+               cpu_ix);
+      const long long max_freq = sys_value_long(path);
+      snprintf(path, sizeof(path),
+               "/sys/devices/system/cpu/cpu%u/cpufreq/scaling_cur_freq",
+               cpu_ix);
+      const long long cur_freq = sys_value_long(path);
+      strb_p_char(&sb, ' ');
+      if (max_freq) {
+        // if available render percent of max frequency
+        const unsigned proc = (unsigned)(cur_freq * 100 / max_freq);
+        total_proc += proc;
+        strb_p_int_with_width(&sb, (int)proc, 3);
+        strb_p_char(&sb, '%');
+      } else {
+        // max frequency not available
+        strb_p(&sb, "----");
+      }
+      cpu_ix++;
+    }
+    pl(sb.chars);
+    strb_clear(&sb);
+  }
+  if (ncpus != 0 && total_proc != 0) {
+    strb_p(&sb, "average: ");
+    strb_p_uint(&sb, (total_proc + (ncpus / 2)) / ncpus);
+    // note: ncpus / 2 for rounding to nearest integer
+    strb_p_char(&sb, '%');
+    pl(sb.chars);
+  }
+}
+
+static void render_battery(void) {
+  if (battery_name[0] == '\0') {
+    // if no battery on the system
+    return;
+  }
+  char path[128] = "";
+  const int nchars =
+      snprintf(path, sizeof(path), "%s%s/%s_", power_supply_path_prefix,
+               battery_name, battery_energy_or_charge_prefix);
+  if (nchars < 0 || (size_t)nchars >= sizeof(path)) {
+    return; // truncated
+  }
+  const size_t maxlen = sizeof(path) - (size_t)nchars;
+  // construct paths to '..._full' and '..._now'
+  char *path_prefix_end = path + nchars;
+  strncpy(path_prefix_end, "full", maxlen);
+  path[sizeof(path) - 1] = '\0'; // ensure null-termination
+  const long long charge_full = sys_value_long(path);
+  strncpy(path_prefix_end, "now", maxlen);
+  path[sizeof(path) - 1] = '\0'; // ensure null-termination
+  const long long charge_now = sys_value_long(path);
+  snprintf(path, sizeof(path), "%s%s/status", power_supply_path_prefix,
+           battery_name);
+  // read battery status
+  char status[64];
+  sys_value_str_line(path, status, sizeof(status));
+  str_to_lower(status);
+  // format output
+  char output[128];
+  snprintf(output, sizeof(output), "battery %u%%  %s",
+           (unsigned)(charge_now * 100 / charge_full), status);
+  // draw a separator for visual que of current battery charge
+  dc_draw_hr1(dc, (int)(WIDTH * charge_now / charge_full));
+  pl(output);
+}
+
+static void render_acpi(void) {
+  FILE *file = popen("acpi -at", "r");
+  if (!file) {
+    return;
+  }
+  unsigned counter = RENDER_ACPI_MAX_LINE_COUNT;
+  while (counter--) {
+    char buf[512] = "";
     if (fscanf(file, "%511[^\n]%*c", buf) == EOF) {
       break;
     }
+    str_to_lower(buf);
     pl(buf);
   }
   pclose(file);
@@ -858,6 +780,80 @@ static void render_bluetooth_connected_devices(void) {
   }
   pclose(file);
 }
+
+static void render_syslog(void) {
+  FILE *file = popen("journalctl -b -p6 -o cat -n 15 --no-pager", "r");
+  if (!file) {
+    return;
+  }
+  unsigned counter = RENDER_SYSLOG_MAX_LINE_COUNT;
+  while (counter--) {
+    char buf[512] = "";
+    if (fscanf(file, "%511[^\n]%*c", buf) == EOF) {
+      break;
+    }
+    pl(buf);
+  }
+  pclose(file);
+}
+
+static void render_cheetsheet(void) {
+  static char *keysheet[] = {"ĸey",
+                             "+c               console",
+                             "+f                 files",
+                             "+e                editor",
+                             "+m                 media",
+                             "+v                 mixer",
+                             "+i              internet",
+                             "+x                sticky",
+                             "+o              binaries",
+                             "+p              snapshot",
+                             "+shift+p          ↳ area",
+                             "",
+                             "đesktop",
+                             "+up                   up",
+                             "+down               down",
+                             "",
+                             "window",
+                             "+esc               close",
+                             "+b                  bump",
+                             "+s                center",
+                             "+w                 wider",
+                             "+W               thinner",
+                             "+r                resize",
+                             "+1           full-screen",
+                             "+2           full-height",
+                             "+3            full-width",
+                             "+right        focus-next",
+                             "+left         focus-prev",
+                             "+shift+up        move-up",
+                             "+shift+down    move-down",
+                             "+0   i-am-bored-surprise",
+                             " ...                ... ",
+                             NULL};
+
+  char **str_ptr = keysheet;
+  while (*str_ptr) {
+    pl(*str_ptr);
+    str_ptr++;
+  }
+}
+
+// static void render_top_10_processes(void) {
+//   FILE *file = popen("ps -eo %mem,%cpu,comm --sort=-%mem | head -n 11", "r");
+//   if (!file) {
+//     return;
+//   }
+//   char buf[512];
+//   unsigned counter = 11;
+//   while (counter--) {
+//     if (fscanf(file, "%511[^\n]%*c", buf) == EOF) {
+//       break;
+//     }
+//     pl(buf);
+//   }
+//   pclose(file);
+// }
 
 static void signal_exit(int i) {
   puts("\nexiting");
