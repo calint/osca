388e8c67b8d406214ed5d7ed90a5aef840dcd937
calint <calin.tenitchi@gmail.com>
2025-11-04 15:34:54 +0700

    .

---
+++ howto/archlinux/2025.11.01--archinstall/install-step-2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+sudo pacman --needed -S xterm xorg-xsetroot xorg-xinput libxft thunar tumbler \
+    mousepad gtk4 rhythmbox mpv alsa-utils scrot feh eog \
+    acpi sysstat brightnessctl ttf-dejavu bluez bluez-utils \
+    gvfs gvfs-mtp iw
+
+cd ~/w/osca
+./init-user-home.sh
+./make-all.sh
