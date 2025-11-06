399757a904337f15597fdf1adfac72facb51ea22
calint <calin.tenitchi@gmail.com>
2025-11-06 22:30:49 +0700

    .

--- init-user-home.sh
+++ init-user-home.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
-cd $(dirname "$0")
+cd "$(dirname "$0")"
 
 echo · copy '~/w/osca/etc/skel/.' to '~/'
 cp -ra ~/w/osca/etc/skel/. ~/
 echo · load dconf from '~/dconf.ini'
-dconf load / < ~/dconf.ini
+dconf load / <~/dconf.ini
 echo · delete '~/dconf.ini'
 rm ~/dconf.ini
