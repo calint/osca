0645b1f37bd1b9ccb2c24b57a0b0b3c554e98c86
calint <calin.tenitchi@gmail.com>
2025-11-10 19:39:13 +0700

    .

--- howto/archlinux/2025.11.01--archinstall/install-step-2.sh
+++ howto/archlinux/2025.11.01--archinstall/install-step-2.sh
@@ -3,8 +3,13 @@ set -e
 
 sudo pacman --needed -S xterm xorg-xsetroot xorg-xinput libxft thunar tumbler \
     mousepad gtk4 rhythmbox mpv alsa-utils scrot feh eog acpi sysstat iw \
-    brightnessctl ttf-dejavu bluez bluez-utils gvfs gvfs-mtp iw
+    brightnessctl ttf-dejavu bluez bluez-utils gvfs gvfs-mtp iw tlp powertop
 
 cd ~/w/osca
 ./init-user-home.sh
 ./make-all.sh
+
+sudo systemctl enable bluetooth
+sudo systemctl start bluetooth
+sudo systemctl enable tlp
+sudo systemctl start tlp
