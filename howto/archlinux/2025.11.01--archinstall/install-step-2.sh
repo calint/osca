86987e13d3e069ec9a91edafcce5c43d1c687e1e
calint <calin.tenitchi@gmail.com>
2025-11-12 12:16:50 +0700

    .

--- howto/archlinux/2025.11.01--archinstall/install-step-2.sh
+++ howto/archlinux/2025.11.01--archinstall/install-step-2.sh
@@ -3,7 +3,7 @@ set -e
 
 sudo pacman --needed -S xterm xorg-xsetroot xorg-xinput libxft thunar tumbler \
     mousepad gtk4 rhythmbox mpv alsa-utils scrot feh eog acpi sysstat iw \
-    brightnessctl ttf-dejavu bluez bluez-utils gvfs gvfs-mtp iw tlp powertop \
+    brightnessctl bluez bluez-utils gvfs gvfs-mtp iw tlp powertop ttf-dejavu \
     ttf-jetbrains-mono-nerd
 
 cd ~/w/osca
