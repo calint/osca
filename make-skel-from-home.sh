f6e92536d01752d8dc46b74c13e5f15becdc8415
calint <calin.tenitchi@gmail.com>
2025-11-13 11:30:14 +0700

    .

--- make-skel-from-home.sh
+++ make-skel-from-home.sh
@@ -7,13 +7,10 @@ SKEL="$(pwd)/etc/skel"
 echo "     source: $HOME"
 echo "destination: $SKEL"
 
-mkdir -p "$SKEL/.config"
 cd "$HOME"
 cp -av .bashrc "$SKEL"
 cp -av .xinitrc "$SKEL"
 cp -av .Xresources "$SKEL"
 cd "$SKEL"
-rm -f .profile
-rm -f .xsession
-ln -s .bashrc .profile
-ln -s .xinitrc .xsession
+ln -sf .bashrc .profile
+ln -sf .xinitrc .xsession
